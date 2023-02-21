c6068fb0ce05ebc42c7ea56c79803f6b7217d541
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-02-21 16:24:54 -0300

    Aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade, FOI MULTADO BROCHA!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
