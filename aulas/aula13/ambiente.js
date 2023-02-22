eea3c4ee44d913fced67c1edfc9fbaf6a9ddf69f
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-02-22 11:24:12 -0300

    Estrutura de Repetição while e do while

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+
+do{
+    console.log(`Passo nº ${c}`)
+    c++
+} while(c<=500)
+
+/* Estrutura de Repetição com teste lógico no início
+
+while (c < 6){
+    console.log(`Passo nº ${c}`)
+    c++
+} 
+
+*/
+
+/* Força Bruta sem repetição, big brain
+ 
+    console.log(`Passo nº ${c}` 1
+    console.log(`Passo nº ${c}` 2
+    console.log(`Passo nº ${c}` 3
+    console.log(`Passo nº ${c}` 4
+    console.log(`Passo nº ${c}` 5
+    console.log(`Passo nº ${c}` 6
+
+*/
