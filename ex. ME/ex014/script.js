142c9efcf0e1e7cf6f655aedf4237e837dded28d
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-02-22 20:39:06 -0300

    p.1 do desafio

---
+++ ex. ME/ex014/script.js
@@ -0,0 +1,11 @@
+function calcular(){
+    var txti = document.getElementById('txtini')
+    var i = Number(txti.value)
+    var txtf = document.getElementById('txtfim')
+    var f = Number(txtf.value)
+    var txtp = document.getElementById('txtpas')
+    var p = Number(txtp.value)
+    var res = document.getElementById('res')
+
+    // estrutura de repetição a ser feita
+}
