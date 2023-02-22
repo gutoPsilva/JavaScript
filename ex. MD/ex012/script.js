29365eef5d4a60b09dc00903a743b99f5dbdf530
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-02-22 19:29:37 -0300

    meio dia e meia noite

--- ex. MD/ex012/script.js
+++ ex. MD/ex012/script.js
@@ -4,16 +4,24 @@ function carregar(){
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    
-    msg.innerHTML = `Agora são ${hora} horas.`
 
-    if(hora >= 4 && hora <= 12){
+    if(hora == 0){
+        msg.innerHTML = `Agora é ${hora}, meia noite.`
+        img.src = 'images/noiteP.png'
+        document.body.style.backgroundColor = 'rgb(7, 28, 48)'
+    }
+    else if(hora >= 4 && hora < 12){
         img.src = 'images/manhaP.png'
+        msg.innerHTML = `Agora são ${hora} horas da manhã.`
+    }else if(hora == 12){
+        msg.innerHTML = `Agora é ${hora}, meio dia.`
     }else if(hora > 12 && hora < 19){
         img.src = 'images/tardeP.png'
+        msg.innerHTML = `Agora são ${hora} horas da tarde.`
         document.body.style.backgroundColor = '#b9846f'
     } else{
         img.src = 'images/noiteP.png'
+        msg.innerHTML = `Agora são ${hora} horas da noite.`
         document.body.style.backgroundColor = 'rgb(7, 28, 48)'
     }
 }
