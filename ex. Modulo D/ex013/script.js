3165bce4bba3e8d5d420de63e0a1401d32417442
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-02-21 22:36:17 -0300

    meio termo desafio

---
+++ ex. Modulo D/ex013/script.js
@@ -0,0 +1,67 @@
+function verificar(){
+    var data = new Date() // pegar data atual
+    var ano  = data.getFullYear() // pegar o ano atual
+    var fAno = document.getElementById('txtano') // guardar o txt do ano
+    var res  = document.querySelector('div#res') // exibir o resultado
+    var idade = ano - fAno.value // calcular idade atual
+
+    if(fAno.value.length != 4 || fAno.value > ano ){ // verifica se os dados são validos, 4 digitos e >=! do atual
+        window.alert('Digite um ano válido.')
+    }
+    else if(idade == ano){ // easter egg jesus christ
+        
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        img.setAttribute('src', 'jesus.jpeg')
+
+        window.alert('Oh my good lord, welcome back Jesus Christ')
+
+    }
+    else{
+        var msex = document.getElementsByName('radsex')
+        var genero = ''
+
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if(msex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade <=12){
+                img.setAttribute('src', 'images/hc.png')
+            }
+            else if(idade >=13 && idade <=21){
+                img.setAttribute('src', 'images/hj.png')
+            }
+            else if( idade >=22 && idade <=50){
+                img.setAttribute('src', 'images/ha.png')
+            }
+            else {
+                img.setAttribute('src', 'images/hv.png')
+            }
+        }
+        else if(msex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade <=12){
+                img.setAttribute('src', 'images/mc.png')
+            }
+            else if(idade >=13 && idade <=21){
+                img.setAttribute('src', 'images/mj.png')
+            }
+            else if( idade >=22 && idade <=50){
+                img.setAttribute('src', 'images/ma.png')
+            }
+            else {
+                img.setAttribute('src', 'images/mv.png')
+            }
+        }
+        res.innerHTML = `Você tem ${idade} anos e é ${genero}`
+        res.style.textAlign= 'center'
+        res.appendChild(img)
+    }
+    
+    
+    /*
+    }else{
+        window.alert(`Sua idade atual é: ${idade}`)
+    }*/
+}
